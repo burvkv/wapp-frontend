7b15e06c86af4a8f1a4e086d48233f6eb0188168
burak.arslan <dev.burvkv@gmail.com>
2021-11-23 15:54:37 +0300

    Wapp - Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './Components/error-page/error-page.component';
+import {ButtonModule} from 'primeng/button';
+import {RippleModule} from 'primeng/ripple';
+import { NavmenuComponent } from './Components/navmenu/navmenu.component';
+import {MenubarModule} from 'primeng/menubar';
+import { SidemenuComponent } from './Components/sidemenu/sidemenu.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent,
+    NavmenuComponent,
+    SidemenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    RippleModule,
+    MenubarModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
