a0beb115371f91660c433b5096a3eee50cb9d1d6
burak.arslan <dev.burvkv@gmail.com>
2021-11-30 15:52:53 +0300

    need breadcumb

--- src/app/Components/breadcrumb-date/breadcrumb-date.component.ts
+++ src/app/Components/breadcrumb-date/breadcrumb-date.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-breadcrumb-date',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbDateComponent implements OnInit {
   
+   cumbs:string[] =["asfsaf","fadsf","gdagad"];
+  
+  activeClass:string="breadcrumb-item active";
+  passiveClass:string="breadcrumb-item";
   constructor() { }
 
   ngOnInit(): void {
